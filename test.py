c8d0957278198e2c18d9a1e29ac4a4e4f30e3071
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2024-04-27 15:01:50 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,97 @@
+import os, pickle, traceback
+from pathlib import Path
+
+
+def get_filepath() -> Path:
+    try:
+        # Get the current working directory
+        current_path = os.getcwd()
+
+        # Define the directory path
+        directory_path = Path(current_path)
+
+        # Concatenate the directory path and dataset location
+        dataset_path = directory_path / 'svm_model.pkl'
+
+    # Check for errors
+    except Exception as e:
+        print('An error occured at get_filepath()')
+        print("Error: ", e)
+
+        # Print the traceback information
+        traceback.print_exc()
+
+        return None
+    
+    # Successful extraction of filepath
+    else:
+        print('Filepaths extracted successfully')
+        return dataset_path
+
+
+def load_model(filepath) -> object:
+    try:
+        with open(filepath, 'rb') as model_file:
+            loaded_model = pickle.load(model_file)
+
+    except Exception as e:
+        print('An error occured at load_model()')
+        print('Error: ', e)
+
+        # Print the traceback information
+        traceback.print_exc()
+
+        return None
+
+    else:
+        print('Dataset loaded successfully')
+        return loaded_model
+
+
+def test_model(loaded_model) -> int:
+    try:
+        test_input = [2.9543,1.076,0.64577,0.89394]
+        predicted_label = loaded_model.predict([test_input])
+    
+    except Exception as e:
+        print('An error occured: ', e)
+        return None
+        
+    else:
+        print('Model predicted successfully')
+        return predicted_label
+
+
+def display_prediction(prediction) -> None:
+    match prediction:
+        case 0:
+            print('Bank Note is Genuine') 
+        case 1:  
+            print('Bank Note is possibly a Counterfeit')
+        case _:
+            print('Unexpected Output occured')
+
+    
+if __name__ == "__main__":
+    filepath = get_filepath()
+
+    # Exit if there is an error getting the filepath
+    if filepath is None:
+        exit(1)
+
+    model = load_model(filepath)
+
+    # Exit if there is an error loading the model
+    if model is None:
+        exit(1)
+
+    prediction = test_model(model)
+
+    # Exit if there is an error predicting the model
+    if prediction is None:
+        exit(1)
+
+    display_prediction(prediction)
+
+
+
